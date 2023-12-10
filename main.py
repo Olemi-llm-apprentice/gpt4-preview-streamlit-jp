8a3186641f7426b37e735dd5c50a33275e9c9722
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2023-12-10 22:24:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+import streamlit as st
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+# openai==0.27.8
+client = OpenAI(api_key=openai_api_key)
+
+st.title("GPT-4 Conversation Bot 💬")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4-1106-preview"
+    # st.session_state["openai_model"] = "ft:gpt-3.5-turbo-1106:tripsero-private-limited::8PDkDBfs"
+    # st.session_state["openai_model"] = "gpt-3.5-turbo-1106"
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    
+    
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            # response_format={ "type": "json_object" },
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            max_tokens = 3500,
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
